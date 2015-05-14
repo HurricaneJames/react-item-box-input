2e9f904f26382dec5c52a1bb848bebbb990e63ff
James Burnett <jtburnett@tribune.com>
2015-05-13 23:00:34 -0400

    item box implementation and testing

---
+++ src/ResizeDetector.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var RESIZE_FRAME_STYLE = {
+  position: 'absolute',
+  visibility: 'hidden',
+  height: '100%',
+  width: '100%'
+};
+
+var ResizeDetector = React.createClass({
+  displayName: 'ResizeDetector',
+  propTypes: {
+    onResize: React.PropTypes.func.isRequired
+  },
+  componentDidMount: function() {
+    var document = this.refs.iframe.getDOMNode().contentDocument;
+    document && (document.defaultView || document.parentWindow).addEventListener('resize', this.handleResize);
+  },
+  componentWillUnmount: function() {
+    var document = this.refs.iframe.getDOMNode().contentDocument;
+    document && (document.defaultView || document.parentWindow).removeEventListener('resize', this.handleResize);
+  },
+  handleResize: function() {
+    this.props.onResize();
+  },
+  render: function() {
+    return <iframe ref='iframe' style={RESIZE_FRAME_STYLE} />;
+  }
+});
+
+module.exports = ResizeDetector;
