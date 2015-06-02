a89fbd1b281595d3e8db42b28050e83a3ae4140f
James Burnett <jtburnett@tribune.com>
2015-06-01 22:31:56 -0400

    update mocha test

--- src/__tests__/ItemBox-test.mocha.js
+++ src/__tests__/ItemBox-test.mocha.js
@@ -7,11 +7,11 @@ var ItemBox = require('../ItemBox');
 describe('ItemBox', function() {
   it('should not blow up', function() {
     var name = 'test';
-    var contributors = Immutable.fromJS([
-      { id: 0, title: 'aaa', affiliateName: 'AAA' },
-      { id: 1, title: 'bbb', affiliateName: 'BBB' }
+    var items = Immutable.fromJS([
+      { id: 0, data: 'aaa' },
+      { id: 1, data: 'bbb' }
     ]);
-    TestUtils.renderIntoDocument(<ItemBox name={name} contributors={contributors} />);
+    TestUtils.renderIntoDocument(<ItemBox name={name} items={items} />);
     // expect this not to have failed
   });
 });
